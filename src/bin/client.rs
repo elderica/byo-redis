a6e2589c63d6d8243527a5aa742c5726b451c5af
elderica <1130138+elderica@users.noreply.github.com>
2023-02-13 22:28:55 +0900

    feat: 04_proto

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,18 +1,61 @@
 use std::{
-    io::{self, Read, Write},
+    io::{self, ErrorKind, Read, Write},
     net::TcpStream,
-    str,
 };
+
+const MAX_MESSAGE: u32 = 4096;
 
 fn main() -> io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:1234")?;
 
-    let msg = b"hello";
-    stream.write_all(msg)?;
+    query(&mut stream, b"hello1")?;
+    query(&mut stream, b"hello2")?;
+    query(&mut stream, b"hello3")?;
 
-    let mut rbuf = [0; 64];
-    let _ = stream.read(&mut rbuf)?;
-    let s = str::from_utf8(&rbuf).unwrap();
-    println!("{s}");
+    Ok(())
+}
+
+fn query(stream: &mut TcpStream, text: &[u8]) -> io::Result<()> {
+    let len = text.len();
+    if len > MAX_MESSAGE as usize {
+        let e = std::io::Error::new(ErrorKind::Other, "too long");
+        return io::Result::Err(e);
+    }
+
+    let mut wbuf = Vec::new();
+    let blen = u32::to_le_bytes(len as u32);
+    wbuf.extend_from_slice(&blen[..4]);
+    wbuf.extend_from_slice(&text[..len]);
+    let r = stream.write_all(&wbuf);
+    if r.is_err() {
+        eprintln!("write_all(wbuf) error");
+        return r;
+    }
+
+    let mut proto_len = [0; 4];
+    let r = stream.read_exact(&mut proto_len);
+    if r.is_err() {
+        eprintln!("read(proto_size) error: {:?}", r.as_ref().err());
+        return r;
+    }
+
+    let len = u32::from_le_bytes(proto_len);
+    if len > MAX_MESSAGE {
+        eprintln!("too long");
+        let e = std::io::Error::new(ErrorKind::Other, "too long");
+        return io::Result::Err(e);
+    }
+
+    // reply body
+    let mut rbuf = vec![0; len as usize];
+    let r = stream.read_exact(&mut rbuf);
+    if r.is_err() {
+        eprintln!("read(reply_body) error");
+        return r;
+    }
+
+    let s = std::str::from_utf8(&rbuf).unwrap();
+    println!("server says: {s}");
+
     Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,30 +1,72 @@
-use std::{
-    io::{self, Read, Write},
-    str,
-};
+use std::io::{self, ErrorKind, Read, Write};
 
 use std::net::{TcpListener, TcpStream};
+
+const MAX_MESSAGE: u32 = 4096;
 
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:1234")?;
 
     for stream in listener.incoming() {
-        do_something(stream?);
+        handle_client(stream?)?;
     }
 
     Ok(())
 }
 
-fn do_something(mut stream: TcpStream) {
-    let mut rbuf = [0; 64];
-    let size = stream.read(&mut rbuf);
-    if size.is_err() {
-        eprintln!("read() error");
-        return;
+fn handle_client(mut stream: TcpStream) -> io::Result<()> {
+    loop {
+        let e = one_request(&mut stream);
+        if e.is_err() {
+            break;
+        }
     }
-    let s = str::from_utf8(&rbuf).unwrap();
+    Ok(())
+}
+
+fn one_request(stream: &mut TcpStream) -> io::Result<()> {
+    let mut proto_len = [0; 4];
+    let r = stream.read_exact(&mut proto_len);
+    if r.is_err() {
+        // eprintln!("read(proto_len) error: {:?}", r.as_ref().err());
+        let e = r.as_ref().err().unwrap();
+        match e.kind() {
+            ErrorKind::UnexpectedEof => eprintln!("EOF"),
+            _ => {
+                eprintln!("read(proto_len) error");
+            }
+        }
+        return r;
+    }
+
+    let len = u32::from_le_bytes(proto_len);
+    if len > MAX_MESSAGE {
+        eprintln!("too long");
+        let e = std::io::Error::new(ErrorKind::Other, "too long");
+        return io::Result::Err(e);
+    }
+
+    // request body
+    let mut rbuf = vec![0; len as usize];
+    let r = stream.read_exact(&mut rbuf);
+    if r.is_err() {
+        eprintln!("read(request_ body) error");
+        return r;
+    }
+
+    // do something
+    let s = std::str::from_utf8(&rbuf).unwrap();
     println!("client says: {s}");
 
-    let wbuf = b"world";
-    stream.write_all(wbuf).unwrap();
+    // reply using the same protocol
+    let reply = b"world";
+    let len = reply.len();
+    let mut wbuf = Vec::new();
+    let proto_len = u32::to_le_bytes(len as u32);
+    wbuf.extend_from_slice(&proto_len[..4]);
+    wbuf.extend_from_slice(reply);
+
+    stream.write_all(&wbuf)?;
+
+    Ok(())
 }
